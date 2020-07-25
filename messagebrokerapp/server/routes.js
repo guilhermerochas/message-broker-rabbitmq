b5de12fc5dd936adbe7fc9aede2e90683f973b3a
Guilherme Rocha <guirochas17@gmail.com>
2020-07-24 21:45:14 -0300

    change route type

--- messagebrokerapp/server/routes.js
+++ messagebrokerapp/server/routes.js
@@ -1,6 +1,6 @@
 const routes = require("express").Router();
 
-routes.post("/send", (req, res) => {
+routes.get("/send", (req, res) => {
   const producer = req.producer;
   producer();
   res.send({ ok: "true" });
